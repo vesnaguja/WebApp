0f37e2dc093d37cc3f84559c102a1a76f87574fb
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-08-15 10:02:44 +0200

    Managed classNames

--- 06my-first-react-project/src/components/BlogList/BlogList.jsx
+++ 06my-first-react-project/src/components/BlogList/BlogList.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { blogs } from "../../data/blog-posts.js";
 import Blog from "../Blog/Blog";
 
 import "./BlogList.css";
 
-export const BlogList = () => {
-  return (
-    <div className="blogList">
-      {
-      blogs.map((post, index) => <Blog post={post} key={index} />)
-      }
-    </div>
-  );
-};
+export class BlogList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.onCountClick = this.onCountClick.bind(this);
+  }
 
+  onCountClick() {
+    this.setState({ count: this.state.count + 1 });
+  }
 
+  render() {
+    const {count} = this.state;
+    return (
+      <div className="blogList">
+        <span>{count}</span> 
+        <button onClick={this.onCountClick}>Click me</button>
+
+        {blogs.map((post, index) => (
+          <Blog post={post} key={index} />
+        ))}
+      </div>
+    );
+  }
+}
--- 06my-first-react-project/src/components/Header/Header.jsx
+++ 06my-first-react-project/src/components/Header/Header.jsx
@@ -1,11 +1,35 @@
+import React, { Component } from "react";
 import "./Header.css";
 
-const Header = () => {
-  return (
-    <header className="header">
-      <h1 className="title">My React Blog</h1>
-    </header>
-  );
-};
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: "Pera",
+    };
+  }
+
+  render() {
+    const { isLoggedIn, user } = this.state;
+
+    let onClickCallback = () => {this.setState({ isLoggedIn: true})}
+    let buttonText = "LogIn";
+
+    if (isLoggedIn) {
+      onClickCallback = () => {this.setState({ isLoggedIn: false})}
+      buttonText = "LogOut";
+    }
+
+    return (
+      <header className="header">
+        <h1 className="title">My React Blog</h1>
+        {isLoggedIn ? <p>{user}</p> : null}
+        <button onClick={onClickCallback}>{buttonText}</button>
+      </header>
+    );
+  }
+}
 
 export default Header;
